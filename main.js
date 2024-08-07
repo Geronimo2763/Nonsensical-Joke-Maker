b3d7686b7f01e08e512408fecee98337f86a6643
Prithiv <budgiedeep37@gmail.com>
2024-08-07 14:00:57 +0530

    created a plan

---
+++ main.js
@@ -0,0 +1,13 @@
+//?PLAN
+/*
+Make a random nonsensical joke generator
+
+-Joke structure: What do you call a {adj} {noun} that is {verb} {adverb} past a {location-noun}? - a {adj} {noun}
+
+-create 3 arrays of adjectives nouns verbs adverbs location-nouns
+
+-choose randomly from these arrays
+
+-print joke the get input then say punchline
+
+*/
